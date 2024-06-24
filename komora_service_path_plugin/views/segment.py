8f758c0a76671d91d3b299d2a139e0c75ac73415
Jan Krupa <jan.krupa@cesnet.cz>
2024-06-24 12:09:54 +0000

    Display Circuits under Segment and ServicePath

--- komora_service_path_plugin/views/segment.py
+++ komora_service_path_plugin/views/segment.py
@@ -1,14 +1,20 @@
 from django.db.models import Count
 
 from netbox.views import generic
-from ..forms import SegmentForm
-from ..models import Segment
-from ..tables import SegmentTable
-from ..filtersets import SegmentFilterSet
+from komora_service_path_plugin.forms import SegmentForm
+from komora_service_path_plugin.models import Segment
+from komora_service_path_plugin.tables import SegmentTable
+# from ..filtersets import SegmentFilterSet
+from circuits.tables import CircuitTable
 
 
 class SegmentView(generic.ObjectView):
     queryset = Segment.objects.all()
+
+    def get_extra_context(self, request, instance):
+        circuits = instance.circuits.all()
+        circuits_table = CircuitTable(circuits, exclude=())
+        return {"circuits_table": circuits_table}
 
 
 class SegmentListView(generic.ObjectListView):
