d4594432c6d288b66e67fb5c3954fa2b602226dd
Jan Krupa <jan.krupa@cesnet.cz>
2024-07-12 09:08:30 +0000

    Display related segments to service path via htmx_table

--- komora_service_path_plugin/views/service_path.py
+++ komora_service_path_plugin/views/service_path.py
@@ -1,24 +1,11 @@
-from django.db.models import Count
-
 from netbox.views import generic
 
-# from ..filtersets import ServicePathFilterSet
-from komora_service_path_plugin.forms import ServicePathForm
-from komora_service_path_plugin.models import ServicePath, ServicePathSegmentMapping
-from komora_service_path_plugin.tables import ServicePathTable, ServicePathSegmentMappingTable
-from circuits.tables import CircuitTable
+from komora_service_path_plugin.models import ServicePath
+from komora_service_path_plugin.tables import ServicePathTable
 
 
 class ServicePathView(generic.ObjectView):
     queryset = ServicePath.objects.all()
-
-    def get_extra_context(self, request, instance):
-        segment_mapping = ServicePathSegmentMapping.objects.filter(
-            service_path=instance.id
-        ).order_by("index")
-        segment_mapping_table = ServicePathSegmentMappingTable(segment_mapping, exclude=("service_path", "id"))
-
-        return {"segment_mapping_table": segment_mapping_table}
 
 
 class ServicePathListView(generic.ObjectListView):
