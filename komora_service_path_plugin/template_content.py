0665157b37ef679a78caa07bd29933f0ddda4842
Jan Krupa <jan.krupa@cesnet.cz>
2024-07-12 08:37:29 +0000

    Move related segments from template_content to ServicePath view

--- komora_service_path_plugin/template_content.py
+++ komora_service_path_plugin/template_content.py
@@ -7,42 +7,41 @@ from utilities.views import ViewTab, register_model_view
 
 from komora_service_path_plugin.models import (
     SegmentCircuitMapping,
-    ServicePathSegmentMapping,
     Segment,
 )
 from komora_service_path_plugin.tables import (
-    ServicePathSegmentMappingTable,
     SegmentTable,
 )
 
 plugin_settings = settings.PLUGINS_CONFIG.get("komora_service_path_plugin", {})
 
 
-class SegmentMappingListToServicePath(PluginTemplateExtension):
-    model = "komora_service_path_plugin.servicepath"
-    exclude = ("service_path", "id")
-
-    def full_width_page(self):
-        service_path = self.context["object"]
-        segment_mapping = ServicePathSegmentMapping.objects.filter(
-            service_path=service_path.id
-        ).order_by("index")
-        segment_mapping_table = ServicePathSegmentMappingTable(
-            segment_mapping, exclude=self.exclude
-        )
-
-        return self.render(
-            "komora_service_path_plugin/segment_mapping_include.html",
-            extra_context={
-                "segment_mapping": segment_mapping,
-                "related_session_table": segment_mapping_table,
-            },
-        )
-
-
-template_extensions = [
-    SegmentMappingListToServicePath,
-]
+# class SegmentMappingListToServicePath(PluginTemplateExtension):
+#    model = "komora_service_path_plugin.servicepath"
+#    exclude = ("service_path", "id")
+#
+#
+#    def full_width_page(self):
+#        service_path = self.context["object"]
+#        segment_mapping = ServicePathSegmentMapping.objects.filter(
+#            service_path=service_path.id
+#        ).order_by("index")
+#        segment_mapping_table = ServicePathSegmentMappingTable(
+#            segment_mapping, exclude=self.exclude
+#        )
+#
+#        return self.render(
+#            "komora_service_path_plugin/segment_mapping_include.html",
+#            extra_context={
+#                "segment_mapping": segment_mapping,
+#                "related_session_table": segment_mapping_table,
+#            },
+#        )
+#
+#
+# template_extensions = [
+#    SegmentMappingListToServicePath,
+# ]
 
 
 @register_model_view(Circuit, name='circuit-komora-segment', path='circuit-komora-segment')
--- komora_service_path_plugin/views/service_path.py
+++ komora_service_path_plugin/views/service_path.py
@@ -4,18 +4,21 @@ from netbox.views import generic
 
 # from ..filtersets import ServicePathFilterSet
 from komora_service_path_plugin.forms import ServicePathForm
-from komora_service_path_plugin.models import ServicePath
-from komora_service_path_plugin.tables import ServicePathTable
+from komora_service_path_plugin.models import ServicePath, ServicePathSegmentMapping
+from komora_service_path_plugin.tables import ServicePathTable, ServicePathSegmentMappingTable
 from circuits.tables import CircuitTable
 
 
 class ServicePathView(generic.ObjectView):
     queryset = ServicePath.objects.all()
 
-    #def get_extra_context(self, request, instance):
-    #    circuits = instance.circuits.all()
-    #    circuits_table = CircuitTable(circuits, exclude=())
-    #    return {"circuits_table": circuits_table}
+    def get_extra_context(self, request, instance):
+        segment_mapping = ServicePathSegmentMapping.objects.filter(
+            service_path=instance.id
+        ).order_by("index")
+        segment_mapping_table = ServicePathSegmentMappingTable(segment_mapping, exclude=("service_path", "id"))
+
+        return {"segment_mapping_table": segment_mapping_table}
 
 
 class ServicePathListView(generic.ObjectListView):
