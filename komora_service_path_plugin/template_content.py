c5c43a65aaadd4958de7a81496d1e33c0683d765
Jan Krupa <jan.krupa@cesnet.cz>
2024-07-25 11:48:25 +0000

    Display circuits and segments to Provider and Tenant

--- komora_service_path_plugin/template_content.py
+++ komora_service_path_plugin/template_content.py
@@ -1,7 +1,8 @@
 from circuits.models import Provider
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from netbox.plugins import PluginTemplateExtension
+
+from komora_service_path_plugin.models import Segment
 
 plugin_settings = settings.PLUGINS_CONFIG.get("komora_service_path_plugin", {})
 
@@ -15,6 +16,15 @@ class CircuitKomoraSegmentExtension(PluginTemplateExtension):
         )
 
 
+class ProviderSegmentExtension(PluginTemplateExtension):
+    model = "circuits.provider"
+
+    def full_width_page(self):
+        return self.render(
+            "komora_service_path_plugin/provider_segments_extension.html",
+        )
+
+
 class TenantProviderExtension(PluginTemplateExtension):
     model = "tenancy.tenant"
 
@@ -24,14 +34,24 @@ class TenantProviderExtension(PluginTemplateExtension):
         ).first()
 
         provider_circuits_count = provider.circuits.count() if provider else None
+        provider_segments_count = (
+            Segment.objects.filter(provider_id=provider.id).count()
+            if provider
+            else None
+        )
 
         return self.render(
             "komora_service_path_plugin/tenant_provider_extension.html",
             extra_context={
                 "provider": provider,
                 "provider_circuits_count": provider_circuits_count,
+                "provider_segments_count": provider_segments_count,
             },
         )
 
 
-template_extensions = [CircuitKomoraSegmentExtension, TenantProviderExtension]
+template_extensions = [
+    CircuitKomoraSegmentExtension,
+    TenantProviderExtension,
+    ProviderSegmentExtension,
+]
