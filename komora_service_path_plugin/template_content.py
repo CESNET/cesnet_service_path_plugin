64c09fe4f2f76e940b991be7b7fdeff11da00694
Jan Krupa <jan.krupa@cesnet.cz>
2024-07-25 10:51:52 +0000

    Add TenantProviderExtension to display provider details in tenant view

--- komora_service_path_plugin/template_content.py
+++ komora_service_path_plugin/template_content.py
@@ -1,4 +1,6 @@
+from circuits.models import Provider
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from netbox.plugins import PluginTemplateExtension
 
 plugin_settings = settings.PLUGINS_CONFIG.get("komora_service_path_plugin", {})
@@ -13,4 +15,23 @@ class CircuitKomoraSegmentExtension(PluginTemplateExtension):
         )
 
 
-template_extensions = [CircuitKomoraSegmentExtension]
+class TenantProviderExtension(PluginTemplateExtension):
+    model = "tenancy.tenant"
+
+    def left_page(self):
+        provider = Provider.objects.filter(
+            custom_field_data__tenant=self.context["object"].pk
+        ).first()
+
+        provider_circuits_count = provider.circuits.count() if provider else None
+
+        return self.render(
+            "komora_service_path_plugin/tenant_provider_extension.html",
+            extra_context={
+                "provider": provider,
+                "provider_circuits_count": provider_circuits_count,
+            },
+        )
+
+
+template_extensions = [CircuitKomoraSegmentExtension, TenantProviderExtension]
