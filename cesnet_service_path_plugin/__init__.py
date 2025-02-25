1dc6a0adaa33a0ffce1444b26f40dbcdf53c4422
Jan Krupa <jan.krupa@cesnet.cz>
2025-02-25 13:57:48 +0000

    Revert "Merge branch '18-nefunkcni-bookmark-a-subscription' into 'master'"
    This reverts merge request !22

--- cesnet_service_path_plugin/__init__.py
+++ cesnet_service_path_plugin/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Cesnet ServicePath Plugin."""
 
 from netbox.plugins import PluginConfig
-from .version import __version__, __author__, __description__, __name__
+from .version import __version__, __author__, __email__, __description__, __name__
 
 
 class CesnetServicePathPluginConfig(PluginConfig):
@@ -9,16 +9,7 @@ class CesnetServicePathPluginConfig(PluginConfig):
     verbose_name = "Cesnet ServicePath Plugin"
     description = __description__
     version = __version__
-    author = __author__
     base_url = "cesnet-service-path-plugin"
-
-    def ready(self):
-        # Call the original ready method
-        super().ready()
-
-        from netbox.models.features import register_models
-
-        register_models(*self.get_models())
 
 
 config = CesnetServicePathPluginConfig
