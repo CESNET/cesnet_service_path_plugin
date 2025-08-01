7178a4ce023de832d94bc9eb02f3f0082181ef9b
Jiri Vrany <jiri.vrany@cesnet.cz>
2025-08-01 10:44:01 +0200

    bump version to 5.0 / breaking change, requires postgis backend to be enabled, database migration fails otherwise.

--- cesnet_service_path_plugin/version.py
+++ cesnet_service_path_plugin/version.py
@@ -1,5 +1,5 @@
 __author__ = "Jan Krupa"
 __email__ = "jan.krupa@cesnet.cz"
-__version__ = "4.3.0"
-__description__ = "Adds ability to create service path in the CESNET network."
+__version__ = "5.0.0"
+__description__ = "Adds ability to create, edit and view service paths in the CESNET network."
 __name__ = "cesnet_service_path_plugin"
